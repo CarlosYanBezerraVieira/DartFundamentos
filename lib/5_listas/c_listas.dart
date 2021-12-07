b58cedba042f6211eb6a95e4390b0fd3ab67bd67
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-12-07 14:27:46 -0300

    list, functions and methods

---
+++ lib/5_listas/c_listas.dart
@@ -0,0 +1,84 @@
+import 'package:dart_fundamentos/1_variaveis/b_nullsafety.dart';
+
+void main() {
+  final numeros = [1, 2, 3];
+  numeros.add(4);
+  //adicionar uma lista de elementos
+  numeros.addAll([5, 6, 7]);
+  print(numeros);
+  final nomes = ["Carlos", "Andréa"];
+  nomes.add("kelvin");
+
+  print(nomes);
+  print(nomes[0]);
+
+  nomes.insert(0, "Yan");
+  print(nomes);
+
+  numeros.remove(1);
+  print(numeros);
+  numeros.removeWhere((numeros) => numeros == 2);
+  print(numeros);
+
+  nomes.removeWhere((nome) {
+    print("nome procurado $nome");
+    if (nome == "Yan") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  print(nomes);
+
+  // print(nomes[0]); <- substituir  isso por
+  print(nomes.first);
+
+  //  print(nomes.length - 1); <- substituir  isso por
+  print(nomes.last);
+
+  print("buscando primeiro nome");
+  var primeiroNome = nomes.firstWhere((nome) {
+    print(nome);
+
+    if (nome == "kelvin") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  print(primeiroNome);
+
+  final numerosGerados = List.generate(10, (index) => index + 1);
+  final stringsGerados = List.generate(10, (index) => "# ${index + 1}");
+  print(numerosGerados);
+  print(stringsGerados);
+  final repeticoes = List.filled(10, "Nada");
+  print(repeticoes);
+
+  print("soma de itens de matrizes");
+  final numerosGeradosParaCalculo = List.generate(100, (index) => index + 1);
+  var soma = numerosGeradosParaCalculo.fold<int>(
+      0, (previousValue, numero) => previousValue = previousValue + numero);
+  print(soma);
+
+  //Spread operator (...)
+  print("Spread operator (...)");
+  var listaNumerosSpreadB = [4, 5, 6];
+  var listaNumerosSpread = [1, 2, 3, ...listaNumerosSpreadB];
+  print(listaNumerosSpread);
+
+  //Colletion if
+  print("//Colletion if");
+  var promocaoAtiva = true;
+  var proodutos = [
+    "Cerveja",
+    "Refrigerante",
+    if (promocaoAtiva) "Suco de laranja"
+  ];
+  print(proodutos);
+
+  //Colletion for
+  var listaInt = [1, 2, 3];
+  var listaString = ["#0", for (var i in listaInt) "#$i"];
+  print(listaString);
+}
